555fe5a765f4d14f020817bce08ef08d93b57fd7
alexander.devaykin <alex.devaykin@gmail.com>
2021-09-16 10:32:53 +0200

    Renames GameLoop::prev_frame_time into prev_frame_duration

--- planets/src/gameloop.rs
+++ planets/src/gameloop.rs
@@ -7,7 +7,7 @@ pub struct GameLoop {
     /// Timestamp recorded when current frame has begun
     frame_start_time: time::Instant,
     /// Time passed since previous frame was started
-    prev_frame_time: time::Duration,
+    prev_frame_duration: time::Duration,
     /// Maximal number of frames per second.
     /// Game loop will wait for required time to pass before starting a new frame to keep constant FPS if needed
     max_fps: i32,
@@ -18,7 +18,7 @@ impl GameLoop {
         GameLoop {
             start_time: time::Instant::now(),
             frame_start_time: time::Instant::now(),
-            prev_frame_time: time::Duration::from_millis(0),
+            prev_frame_duration: time::Duration::from_millis(0),
             max_fps: 120,
         }
     }
@@ -30,12 +30,12 @@ impl GameLoop {
             thread::sleep(wanted_time_per_frame - self.frame_start_time.elapsed());
         }
 
-        self.prev_frame_time = self.frame_start_time.elapsed();
+        self.prev_frame_duration = self.frame_start_time.elapsed();
         self.frame_start_time = time::Instant::now();
     }
 
     pub fn get_prev_frame_time(&self) -> time::Duration {
-        self.prev_frame_time
+        self.prev_frame_duration
     }
 
     pub fn set_max_fps(&mut self, max_fps: i32) {
