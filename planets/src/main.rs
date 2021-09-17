4cb3d05163f59f0b44461bfea14dee909744e04f
Sergey <sergeymaxin@yandex.ru>
2021-09-17 21:47:46 +0200

    Sun Module added. Sun aging fn.

--- planets/src/main.rs
+++ planets/src/main.rs
@@ -1,13 +1,19 @@
 mod gameloop;
+mod sun;
 
 use crate::gameloop::GameLoop;
+use crate::sun::Sun;
 
 fn main() {
     println!("Hello, world!");
 
     let mut gameloop = GameLoop::new();
     gameloop.set_max_fps(2);
+    
     let mut fame_num = 0 as u64;
+    
+    let mut sun = Sun::new();
+
     loop {
         // Notify frame start
         gameloop.start_frame();
@@ -15,8 +21,13 @@ fn main() {
         // All the game logic entry point is here.
         // Update single planet block object here using e.g. gameloop.get_prev_frame_time() function to get passed time
         println!("Frame {} started.", fame_num);
-
+        
+        sun.sun_age_updt(gameloop.get_prev_frame_time());
+        /// Что такое {:?}
+        println!("Sun age is {}", sun.get_sun_age().as_millis()); 
+        
         // Increase frame count in the end
         fame_num += 1;
+        
     }
 }
