98b041ae8b40028c5f595138a821095a5d3a7343
alexander.devaykin <alex.devaykin@gmail.com>
2021-09-30 22:33:42 +0200

    VS Code settings for shader compilation task

--- planets/shaders/compile.bat
+++ planets/shaders/compile.bat
@@ -1,5 +1,7 @@
-%VULKAN_SDK%\Bin32\glslc.exe basic.vert -o bin/basic.vert.spv
-%VULKAN_SDK%\Bin32\glslc.exe basic.frag -o bin/basic.frag.spv
+%cd%
 
-%VULKAN_SDK%\Bin32\glslc.exe background.vert -o bin/background.vert.spv
-%VULKAN_SDK%\Bin32\glslc.exe background.frag -o bin/background.frag.spv
+%VULKAN_SDK%\Bin32\glslc.exe shaders\basic.vert -o shaders\bin\basic.vert.spv
+%VULKAN_SDK%\Bin32\glslc.exe shaders\basic.frag -o shaders\bin\basic.frag.spv
+
+%VULKAN_SDK%\Bin32\glslc.exe shaders\background.vert -o shaders\bin\background.vert.spv
+%VULKAN_SDK%\Bin32\glslc.exe shaders\background.frag -o shaders\bin\background.frag.spv
