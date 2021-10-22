8d3c4ca8afa3f7d2c04d8a17bb040bc2f2e016e2
chakiflyer <sergeymaxin@yandex.ru>
2021-10-22 21:08:29 +0200

    bin32 / shader compile fix

--- planets/shaders/compile.bat
+++ planets/shaders/compile.bat
@@ -1,13 +1,13 @@
 @echo Working directory: %cd%
 
 @echo Compiling basic.vert
-@%VULKAN_SDK%\Bin32\glslc.exe %cd%\shaders\basic.vert -o %cd%\shaders\bin\basic.vert.spv
+@%VULKAN_SDK%\Bin\glslc.exe %cd%\shaders\basic.vert -o %cd%\shaders\bin\basic.vert.spv
 @echo Compiling basic.frag
-@%VULKAN_SDK%\Bin32\glslc.exe %cd%\shaders\basic.frag -o %cd%\shaders\bin\basic.frag.spv
+@%VULKAN_SDK%\Bin\glslc.exe %cd%\shaders\basic.frag -o %cd%\shaders\bin\basic.frag.spv
 
 @echo Compiling background.vert
-@%VULKAN_SDK%\Bin32\glslc.exe %cd%\shaders\background.vert -o %cd%\shaders\bin\background.vert.spv
+@%VULKAN_SDK%\Bin\glslc.exe %cd%\shaders\background.vert -o %cd%\shaders\bin\background.vert.spv
 @echo Compiling background.frag
-@%VULKAN_SDK%\Bin32\glslc.exe %cd%\shaders\background.frag -o %cd%\shaders\bin\background.frag.spv
+@%VULKAN_SDK%\Bin\glslc.exe %cd%\shaders\background.frag -o %cd%\shaders\bin\background.frag.spv
 
 @echo Done shaders compilation
